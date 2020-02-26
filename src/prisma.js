f78f78267853fb10e8d05313ead413bcb802c7b3
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-02-26 15:59:59 +0200

    mutations with prisma bindings

--- src/prisma.js
+++ src/prisma.js
@@ -6,10 +6,44 @@ const prisma = new Prisma({
 
 // prisma.query  prisma.mutation  prisma.subscription  prisma.exists
 
-prisma.query.users(null, '{id name posts { id title }}').then((data) => {
+// prisma.query.users(null, '{id name posts { id title }}').then((data) => {
+//   console.log(JSON.stringify(data, undefined, 4))
+// })
+
+// prisma.query.comments(null, '{id text author {name} post {id}} ').then((data) => {
+//   console.log(JSON.stringify(data, undefined, 4))
+// })
+
+// prisma.mutation.createPost({
+//   data: {
+//     title: "GraphQL 101",
+//     body: "",
+//     published: false,
+//     author: {
+//       connect: {
+//         id: "ck736e1rc00qk0863eggqyh1o"
+//       }
+//     }
+//   }
+// }, '{ id title body published}').then((data) => {
+//   console.log(data)
+//   return prisma.query.users(null, '{id name posts {id title}}')
+// }).then((data) => {
+//   console.log(JSON.stringify(data, undefined, 4))
+// })
+
+prisma.mutation.updatePost({
+  where: {
+    id: "ck73co7wh03kj0863uszhmera"
+  },
+  data: {
+    published: true,
+    body: "newly changed post **"
+  }
+}, '{ id }').then((data) => {
+  console.log(data)
+  return prisma.query.users(null, '{id name posts {id title}}')
+}).then((data) => {
   console.log(JSON.stringify(data, undefined, 4))
 })
 
-prisma.query.comments(null, '{id text author {name} post {id}} ').then((data) => {
-  console.log(JSON.stringify(data, undefined, 4))
-})
