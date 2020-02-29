a419dd06dc6cb304cd05ae53343ac50744e20fef
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-02-29 11:49:17 +0200

    locking down queries II

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -13,33 +13,54 @@ const Query = {
         }]
       }
     }
-
     return prisma.query.users(opArgs, info)
-
   },
-  posts(parent, args, { prisma }, info) {
-    const opArgs = {}
 
-    if (args.query) {
-      opArgs.where = {
-        OR: [{
-          title_contains: args.query
-        },
-        {
-          body_contains: args.query
-        }]
+  posts(parent, args, { prisma }, info) {
+    const opArgs = {
+      where: {
+        published: true
       }
     }
 
+    if (args.query) {
+      opArgs.where.OR = [{
+        title_contains: args.query
+      },
+      {
+        body_contains: args.query
+      }]
+    }
     return prisma.query.posts(opArgs, info)
+  },
 
+  async myPosts(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request)
+
+    const opArgs = {
+      where: {
+        author: {
+          id: userId
+        }
+      }
+    }
+
+    if (args.query) {
+      opArgs.where.OR = [{
+        title_contains: args.query
+      },
+      {
+        body_contains: args.query
+      }]
+    }
+    return prisma.query.posts(opArgs, info)
   },
 
   comments(parent, args, { prisma }, info) {
     return prisma.query.comments(null, info)
   },
 
-  async me(parent, args, {prisma, request}, info) {
+  async me(parent, args, { prisma, request }, info) {
     const userId = getUserId(request)
 
     return prisma.query.user({
